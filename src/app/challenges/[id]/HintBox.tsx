6b9a734b2adf44259108f0397046f0c031596a77
Rebecca <bex.wood@softwire.com>
2024-07-23 10:46:18 +0100

    Refactor HintBox component

--- src/app/challenges/[id]/HintBox.tsx
+++ src/app/challenges/[id]/HintBox.tsx
@@ -25,22 +25,30 @@ export default function HintBox({ challenge }: { challenge: Challenge }) {
   const [hintIndex, setHintIndex] = useState<number | undefined>(undefined)
   const maxHintIndex = challenge.hints.length - 1
 
-  if (hintIndex === undefined) {
-    return (
-    <div className="flex flex-row rounded border border-teal-900 p-3 mt-5">
-      <h3 className="flex grow justify-center px-3 text-center">
-        <p>Do you need a hint?</p>
-      </h3>
-      <Button label={'>'} loading={false} onClick={() => setHintIndex(0)}/>
-    </div>
-    )
-  }
-
   return (
     <div className="flex flex-row rounded border border-teal-900 p-3 mt-5 grid-cols-3">
-      {hintIndex > 0 && <Button label={'<'} loading={false} onClick={() => setHintIndex(hintIndex - 1)} />}
-      <h3 className="flex grow justify-center px-3 text-center">{renderHint(challenge.hints, hintIndex)}</h3>
-      {hintIndex < maxHintIndex && <Button label={'>'} loading={false} onClick={() => setHintIndex(hintIndex + 1)} />}
+      {hintIndex !== undefined && hintIndex > 0 && (
+        <Button
+          label={'<'}
+          loading={false}
+          onClick={() => setHintIndex(hintIndex - 1)}
+        />
+      )}
+      <h3 className="flex grow justify-center px-3 text-center">
+        {hintIndex === undefined
+          ? <p>Do you need a hint?</p>
+          : renderHint(challenge.hints, hintIndex)
+        }
+      </h3>
+      {(hintIndex === undefined || hintIndex < maxHintIndex) && (
+        <Button
+          label={'>'}
+          loading={false}
+          onClick={() =>
+            setHintIndex(hintIndex === undefined ? 0 : hintIndex + 1)
+          }
+        />
+      )}
     </div>
   );
 }
